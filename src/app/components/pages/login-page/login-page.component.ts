49b57eac63de50cb0bce89c4050f8c4201d92b17
Dariohg <rubendarioh99@gmail.com>
2024-11-11 16:17:01 -0600

    css

--- src/app/components/pages/login-page/login-page.component.ts
+++ src/app/components/pages/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../../../services/auth.service';
 import { TitleComponent } from '../../atoms/title/title.component';
 import { LoginFormComponent } from '../../organisms/login-form/login-form.component';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router'; // Importa Router
 
 @Component({
   selector: 'app-login-page',
@@ -12,7 +13,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {} // Inyecta Router
 
   onFormSubmit(loginData: { username: string; password: string }): void {
     this.authService.login(loginData.username, loginData.password).subscribe(
@@ -20,11 +21,13 @@ export class LoginPageComponent {
         console.log('Login exitoso:', response);
 
         // Guarda el token en localStorage
-        const token = response.token; // Ajusta según el nombre de la propiedad en la respuesta
+        const token = response.token;
         if (token) {
           localStorage.setItem('token', token);
           console.log('Token guardado en localStorage');
-          // Opcional: Redirecciona a otra página si es necesario
+
+          // Redirige a la página /dogs
+          this.router.navigate(['/dogs']);
         } else {
           console.error('No se recibió un token en la respuesta');
         }
